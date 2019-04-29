af050ffe3a6b32e6f3fa2e3c39772dd6c9fdc3c4
Neil Gunning <43641172+ngunning@users.noreply.github.com>
2019-04-29 05:52:28 +0100

    Add files via upload

---
+++ PlayerTracker/Accel_GPS_Wifi_Database_v1/Wifi.ino
@@ -0,0 +1,19 @@
+/**************** WiFi (Print status) ********************/
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // print where to go in the browser
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
+}
